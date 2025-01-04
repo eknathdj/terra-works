c91bf7eb80afda99066d2cd899a9c0ca79d33caa
Eknathdj <eknath789@gmail.com>
2025-01-04 16:29:10 +0530

    added main file

--- IaC-1/providers.tf
+++ IaC-1/providers.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~>1.5"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.14.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
+  subscription_id = "06a8bf7d-a918-4bc6-8da4-d6033f25855b"
 }
